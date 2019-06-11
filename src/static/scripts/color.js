5567183aa5e2a1b118843e2fa8c20abc65704ec4
tphuc <phuccokerml@gmail.com>
2019-06-11 13:55:17 +0700

    empty branch

--- src/static/scripts/color.js
+++
@@ -1,6 +0,0 @@
-export const mainDark = '#2d333d';
-export const secondaryDark = '#3c4452';
-export const mainLight = '#e0f2f1';
-export const secondaryLight = '#e0e0e0';
-export const darkDra = '#1e232b';
-export const mainWhite = '#eceff1'
